3b3ae1f07bc40f6bf86a9e5a3856baa8a16624a6
NahuelCerrato <nahuelcerrato@gmail.com>
2025-04-17 15:31:48 -0300

    Se agregó Ejercicio 1

---
+++ 04 Estructuras Repetitivas/Ejercicio 1 - TP 4.py
@@ -0,0 +1,5 @@
+#1) Crea un programa que imprima en pantalla todos los números enteros desde 0 hasta 100
+#(incluyendo ambos extremos), en orden creciente, mostrando un número por línea.
+x = 0
+for x in range(0,101):
+    print(x)
