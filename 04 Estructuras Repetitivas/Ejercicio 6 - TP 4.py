e0e545eb04ebd6315bd0f1736858e2a516e8daf1
NahuelCerrato <nahuelcerrato@gmail.com>
2025-04-20 18:49:57 -0300

    Se agregó Ejercicio 6

---
+++ 04 Estructuras Repetitivas/Ejercicio 6 - TP 4.py
@@ -0,0 +1,8 @@
+#6) Desarrolla un programa que imprima en pantalla todos los números pares comprendidos
+#entre 0 y 100, en orden decreciente.
+
+for c in range(100, 0, -2):
+    if c % 2 == 0:
+        print(c)
+
+
