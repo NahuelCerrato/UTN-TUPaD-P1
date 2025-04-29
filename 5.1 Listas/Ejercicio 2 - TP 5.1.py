2d7299a0e5f59a6a8045700ea76ea3843b9c4e1b
NahuelCerrato <nahuelcerrato@gmail.com>
2025-04-29 11:57:42 -0300

    Se agregó Ejercicio 2.

---
+++ 5.1 Listas/Ejercicio 2 - TP 5.1.py
@@ -0,0 +1,6 @@
+#2) Crear una lista con cinco elementos (colocar los elementos que más te gusten) y mostrar el
+#penúltimo. ¡Puedes hacerlo como se muestra en los videos o bien investigar cómo funciona el
+#indexing con números negativos!
+
+actividades = [ "Fotografía", "Gimnasio","Mates", "Motos", "Informática"]
+print(f" El penúltimo elemento es: {actividades[-2]}")
