012b8f361c7a7e6164bff5cc2b671048032f01ec
NahuelCerrato <nahuelcerrato@gmail.com>
2025-04-29 12:21:11 -0300

    Se agregó ejercicio 7

---
+++ 5.1 Listas/Ejercicio 7 - TP 5.1.py
@@ -0,0 +1,9 @@
+#7) Reemplazar los dos valores centrales (índices 1 y 2) de la lista “autos” por dos nuevos valores
+#cualesquiera.
+
+
+autos = ["sedan", "polo", "suran", "gol"]
+print(f"Lista: {autos}")
+autos[1] = "golf"
+autos[2] = "coupe"
+print(f"Lista nueva: {autos}")
