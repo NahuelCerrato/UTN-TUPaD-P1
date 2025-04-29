8698d90fb4c5185ad5d924e26abe1b6a406f9bc8
NahuelCerrato <nahuelcerrato@gmail.com>
2025-04-29 12:10:55 -0300

    Se agregó Ejercicio 5.

---
+++ 5.1 Listas/Ejercicio 5 - TP 5.1.py
@@ -0,0 +1,7 @@
+#5) Analizar el siguiente programa y explicar con tus palabras qué es lo que realiza.
+
+#numeros = [8, 15, 3, 22, 7]
+#numeros.remove(max(numeros))
+#print(numeros)
+
+#El programa quita el valor mas alto de los elementos de la lista, sin importar el orden en el que estén los valores.
